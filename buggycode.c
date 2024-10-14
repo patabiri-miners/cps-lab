89852d3969ad2d110a1c1dc37bc4ab069767a52a
patabiri-miners <patabiri@miners.utep.edu>
2024-10-14 16:14:20 -0600

    debug work

---
+++ buggycode.c
@@ -0,0 +1,20 @@
+//Debugged code by: Patrick Tabiri
+
+#include <stdio.h>
+
+int main() {
+
+ float fahrenheit;
+
+ printf("Enter temperature in Fahrenheit: ");
+
+ scanf("%f", &fahrenheit);
+ 
+
+ float celsius = (fahrenheit - 32) * 5 / 9;
+
+ printf("Temperature in Celsius: %f\n", celsius);
+
+ return 0;
+
+}
